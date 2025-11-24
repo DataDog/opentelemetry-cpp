f35993718147ed6d4db58dc6d23b483f4519db9b
Zach Montoya <zach.montoya@datadoghq.com>
2025-11-24 10:43:02 -0800

    ## Proposed changes by zach.montoya:
     ### Update the Datadog trace provider's GetTracer implementation to return the shared static Tracer instance.
    
    This is because all tracers obtained by GetTracer share the same configuration and active trace context. The only functional difference when using different tracers is that spans created by the tracer will be in a tracer-specific Instrumentation Scope (name, version, schema_url) -- see proto here https://github.com/open-telemetry/opentelemetry-proto/blob/a1c9c50af2f62c3fc7ea17e3aeec8c911b2c5e08/opentelemetry/proto/common/v1/common.proto#L76. The Datadog tracer doesn't support this concept at this time.
    
     ### Update Tracer::StartSpan
    Update the "IsRootSpan" check with a call to `opentelemetry::trace::IsRootSpan(Context)` since it is part of the tracing API
    
     ### Add to dependencies doc
    Adds the Datadog exporter as an external dependency in the documentation
    
     ## Notes for later
    
     ### No Update - SetAttributes
    We will need to add logic to convert the attribute value (AnyValue) to a string, likely replicating all of the branches of OtlpPopulateAttributeUtils::PopulateAnyValue seen in `exporters/otlp/src/otlp_populate_attribute_utils.cc`

--- exporters/datadog/test/provider_test.cc
+++ exporters/datadog/test/provider_test.cc
@@ -7,23 +7,19 @@
 
 using namespace OPENTELEMETRY_NAMESPACE;
 
-TEST(DatadogProvider, DifferentNameYieldDifferentTracerInstances)
+// TODO: Update this when the Datadog tracer supports instrumentation scopes.
+// For now, regardless of the GetTracer inputs, the same tracer instance is returned.
+TEST(DatadogProvider, GetTracer)
 {
   exporter::datadog::TracerProvider provider;
   auto t1 = provider.GetTracer("t1");
-  ASSERT_TRUE(t1 != nullptr);
-  auto t2 = provider.GetTracer("t2");
-  ASSERT_TRUE(t2 != nullptr);
-
-  EXPECT_NE(t1, t2);
-}
-
-TEST(DatadogProvider, SameNameYieldTheSameTracerInstance)
-{
-  exporter::datadog::TracerProvider provider;
-  auto t1 = provider.GetTracer("t1");
-
   auto t1_prime = provider.GetTracer("t1");
-  ASSERT_TRUE(t1 != nullptr);
+  auto t2 = provider.GetTracer("t2");
+
+  ASSERT_NE(nullptr, t1);
+  ASSERT_NE(nullptr, t1_prime);
+  ASSERT_NE(nullptr, t2);
+
   EXPECT_EQ(t1, t1_prime);
+  EXPECT_EQ(t1, t2); // TODO: When fixed, assert that t1 != t2
 }
